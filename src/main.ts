0d2ff24e419322cffccfb350b7c1f2ff1608445c
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2022-07-17 23:01:41 +0300

    feat: add swagger-ui

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+
+import { AppModule } from './app.module';
 
 import { config } from 'dotenv';
 
@@ -8,7 +13,15 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 4000;
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+
+  const apiDocFilePath = resolve(process.cwd(), 'doc', 'api.yaml');
+  const apiDoc = await readFile(apiDocFilePath, 'utf8');
+
+  SwaggerModule.setup('doc', app, parse(apiDoc));
+
+  await app.listen(port);
 }
 bootstrap();
